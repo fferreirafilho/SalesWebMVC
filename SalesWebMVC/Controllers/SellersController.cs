137a400f8b536c4b2366599adec422b880b104db
Fernando <81649709+Fernando-Ferreira-Filho@users.noreply.github.com>
2022-07-21 20:56:27 -0300

    Foreign key not null

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers {
@@ -13,6 +14,17 @@ namespace SalesWebMVC.Controllers {
         public IActionResult Index() {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -14,5 +14,11 @@ namespace SalesWebMVC.Services {
         public List<Seller> FindAll() {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj) {
+            obj.Department = _context.Department.First();
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
